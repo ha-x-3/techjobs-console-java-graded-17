da7acba50bff196f485311f81cf3a8d589b2ac3d
Brittney Epperson <beppers87@hotmail.com>
2023-08-30 11:39:17 -0500

    Minor adjustment for autograding

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -128,7 +128,7 @@ public class TechJobs {
                 System.out.println("*****");
             }
         } else {
-            System.out.println("No Results");
+            System.out.print("No Results");
         }
     }
 }
